eaa0945811d73609b178b0f6f030f75eb0e76a7d
ppaulohs4-dotcom <ppaulohs4@gmail.com>
2026-09-18 17:48:42 -0300

    Create exerciciosfacul.py

---
+++ exerciciosfacul.py
@@ -0,0 +1,167 @@
+ #exercicio 3 (1)
+#lado = float(input("Coloque o valor do lado do quadrado: "))
+#perimetro = lado * 4
+#print ("O seu perimetro é: ", perimetro)
+ 
+ #exercicio 4 (1)
+#numero = float(input("Me de um númro: "))
+#total = numero * 2
+#print ("Esse é o dobro do seu número:",total)
+
+ #exercicio 5 (1)
+#base = float(input("Me de a base do retangulo: "))
+#altura = float(input("Me de a altura do retangulo: "))
+#total = base * altura
+#print ("Essa é sua area:",total)
+
+ #exercicio 6 (1)
+#C = float(input("Coloque a temperatura em Celsius: "))
+#F = (C * 1.8) + 32
+#print ("Em Fahrenheit:", F)
+
+ #exercicio 7 (1)
+#h = int(input("Quantas horas voce trabalhou: "))
+#s = float(input("Quanto voce recebe por hora: "))
+#total = s * h
+#print ("Seu salario final será:",total)
+ 
+ #exercicio 8 (1)
+#r = float(input("Tem quantos reais: "))
+#dollar = r / 5.13
+#print ("Sua quantidade de dollars é:",dollar)
+
+ #exercicio 1 (2)
+#n1 = int(input("Quero um número inteiro: "))
+#n2 = int(input("Quero outro número inteiro: "))
+#soma = (n1+n2)
+#if soma>20:
+#    print ("true")
+#else:
+#    print ("false")
+
+ #exercicio 2 (2)
+#pesoc = float(input("Qual o peso da caixa: "))
+#pesoo = float(input("Qual o peso do objeto: "))
+#soma = (pesoc+pesoo)
+#if soma<=50:
+#    print ("true")
+#else:
+#    print ("false")
+
+ #exercicio 3 (2)
+#remedio = (100)
+#vendido = int(input("Quantas unidades de remedio foram vendidos? "))
+#estoque = remedio - vendido
+#if estoque>19:
+#    print ("isso mesmo")
+#else:
+#    print ("muito ruim, melhore")
+
+ #exercicio 4 (2)
+#limite = 80
+#velocidade = float(input("Qual a velocidade do carro? Em km/h: ")) 
+#if velocidade > limite:
+#    print ("multado")
+#else:
+#    print ("pode continuar dirigindo")
+
+ #exercicio 5 (2)
+#nota = float(input("Qual foi a sua nota? "))
+#frequencia = int(input("Qual sua frequencia em %? "))
+#if nota >= 6 and frequencia >= 75:
+#    print ("Aluno aprovado")
+#else:
+#    print ("Aluno reprovado")
+
+#exercicio 1 (3)
+#idade = int(input("Digite sua idade: "))
+
+#if idade >= 16:
+#  print ("Pode votar")
+#else:
+#  print ("Não pode votar")
+
+# exercicio 2 (3)
+#numero = float(input("Digite um numero: "))
+
+#if numero >= 0:
+#  print ("Seu número é positivo")
+#else:
+#  print ("Seu número é negativo")
+
+#exercício 3 (3)
+#idade = int(input("Digite sua idade: "))
+
+#if idade >= 18:
+#  CNH = input("Voce possui cnh? ")
+  
+#  if CNH == "sim":
+#       print("Pode dirigir")
+
+#  else:
+#       print("Tire a carteira")
+
+#else:
+#   print("Não pode dirigir")
+
+# exercício 4 (3)
+#numero = int(input("Escreva um numero inteiro: "))
+
+#if numero >= 1:
+#    if numero % 2 == 0:
+#      print("Numero positivo e par")
+#    else:
+#      print("Numero positvo e impar")
+
+#else:
+#   if numero < 0:
+#      print("Numero negativo")
+#   else:
+#      print("Zero nao é positivo e nem negativo")
+
+#exercicio 5 (3)
+#numeroint = int(input("Me de um numero inteiro: "))
+#if numeroint % 2 == 0:
+#    print ("Seu numero é par")
+#else:
+#   print ("Seu numero é impar")
+    
+#number1 = int(input("Me diga um número inteiro: "))
+#number2 = int(input("Outro número: "))
+#if number1 > number2:
+#    print ("O primeiro número é maior")
+#elif number1 < number2:
+#    print ("O segundo número é maior")
+#else:
+#    print ("Ambos são iguais") 
+
+#exercicio 6 (3)
+#usuario = str(input("Nome de usuário: "))
+#senha = int(input("Digite a senha: "))
+#if usuario == "unisa" and senha == 1234:
+#    adm = str(input("Deseja acessar como adm? "))
+#    if adm == "sim":
+#        print ("Acesso total")
+#    else:
+#        print ("Acesso restrito")
+#elif usuario == "usuario" and senha == 5678:
+#    print ("Acesso externo")
+#else:
+#    print ("usuario ou senha invalidos")
+
+#media = float(input("Qual a sua média? "))
+#frequencia = int(input("Qual a sua frequencia em %? "))
+#if media >= 7:
+#    if frequencia >= 75:
+#        print ("Aprovado")
+#    else:
+#        print ("Reprovado por falta")
+                 
+#else:
+#    print ("Reprovado por nota")
+#if media >= 11 or media <=-1:
+#    print ("Erro")
+#elif frequencia >= 101 or frequencia <=-1:
+#    print ("Erro")
+    
+
